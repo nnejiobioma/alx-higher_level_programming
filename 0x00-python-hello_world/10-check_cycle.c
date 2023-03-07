fb944764586977570df9bbfcdd241a4927db6ca4
nnejiobioma <nnejiobioma@gmail.com>
2023-03-07 20:17:48 +0000

    Project file activated

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* check_cycle - checks if a singly linked list has a cycle in it
+* @list: list head
+* Return: 0 if no, 1 if yes
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow = list;
+
+	if (list == NULL)
+	return (0);
+
+	fast = list->next;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+	if (slow == fast)
+	return (1);
+	fast = fast->next->next;
+	slow = slow->next;
+	}
+	return (0);
+}
