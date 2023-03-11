26a3c8afb0b3569efd8ffe8f6c0a70c78f1addbc
nnejiobioma <nnejiobioma@gmail.com>
2023-03-11 16:41:10 +0000

    task five completed

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    list_chars = list(my_string)
+    for char in list_chars:
+        if char == 'c' or char == 'C':
+            list_chars.remove(char)
+    return("".join(list_chars))
