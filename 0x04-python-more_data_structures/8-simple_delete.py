e7d34f12fa6b0de6bc7cf2b36840a10e9297927a
nnejiobioma <nnejiobioma@gmail.com>
2023-03-18 10:40:23 +0000

    Amended task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+def simple_delete(new_dict, key=""):
+    if key in new_dict:
+        del new_dict[key]
+    return new_dict
