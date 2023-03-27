c4a90787306b4cdcd68a48bfe4a4bcd6f9bea918
nnejiobioma <nnejiobioma@gmail.com>
2023-03-27 21:05:17 +0000

    tast one amended

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-        r_num = 0
-        for i in range(x):
-            try:
-                print(my_list[i], end="")
-                r_num += 1
-            except IndexError:
-                break
-        print()
-       return r_num
+    i = 0
+    while i < x:
+        try:
+            print("{}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        i = i + 1
+    print("", end="\n")
+    return (i)
