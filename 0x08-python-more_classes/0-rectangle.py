00181dfb27b35b0f0f9200c1dc07fc3b2f74ff93
nnejiobioma <nnejiobioma@gmail.com>
2023-04-05 17:02:55 +0000

    my first push

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Write an empty class Rectangle
+"""
+
+class Rectangle:
+    """This is an empty class Rectangle"""
+    print(Rectangle)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is my read me file for project 0x08-python-more_classes
