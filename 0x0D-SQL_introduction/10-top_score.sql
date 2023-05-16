de7eb9c53205bd177bc5b83eaa5f1b87f1671df2
nnejiobioma <nnejiobioma@gmail.com>
2023-05-16 21:51:46 +0000

    more task added

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- This script list all the records of the table second_table
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+--This is a script that diplays all the reecords with a score >= 10 
+SELECT score, name FROM second_table ORDER BY score DESC;
