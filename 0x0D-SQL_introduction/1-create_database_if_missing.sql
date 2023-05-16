ef263a03744b7775bff3843f979049ffe4391714
nnejiobioma <nnejiobioma@gmail.com>
2023-05-16 21:40:20 +0000

    made changes to code

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,6 @@
---This creates a data base hbtn_oc_0. the data base if created even if the file already exists--
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+--This creates a data base hbtn_oc_0. the data base if created even if the file already exists
+
+db_name="hbtn_0c_0"
+
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $db_name;"
