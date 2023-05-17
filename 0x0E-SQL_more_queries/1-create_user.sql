9b0ac37b7be4b604f28ed26f8ccdb39ae2e76210
nnejiobioma <nnejiobioma@gmail.com>
2023-05-17 20:31:38 +0000

    tast one handled

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,12 @@
+-- This script creates a user user_0d_1
+-- Grants all privillages
+-- Users IDENTIFIED BY 
+-- Still runs even if the user name already exists
+
+CREATE USER 
+    IF NOT EXISTS 'user_0d_1'@'localhost' 
+    IDENTIFIED BY 'user_0d_1_pwd'; 
+
+GRANT ALL PRIVILEGES 
+    ON *.* 
+    TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
