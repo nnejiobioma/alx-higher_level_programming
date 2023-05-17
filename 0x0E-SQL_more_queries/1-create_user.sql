e4c22b673907c0375bde0ceed1196c255ab7d302
nnejiobioma <nnejiobioma@gmail.com>
2023-05-17 20:33:40 +0000

    make changes to code

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,10 +3,4 @@
 -- Users IDENTIFIED BY 
 -- Still runs even if the user name already exists
 
-CREATE USER 
-    IF NOT EXISTS 'user_0d_1'@'localhost' 
-    IDENTIFIED BY 'user_0d_1_pwd'; 
-
-GRANT ALL PRIVILEGES 
-    ON *.* 
-    TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
