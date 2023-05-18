e7dc701e23842670120c8260af3cae5463b01eb6
nnejiobioma <nnejiobioma@gmail.com>
2023-05-18 20:59:23 +0000

    task 6 amended again

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
---This script that creates the database hbtn_0d_usa and the 
+-- This script that creates the database hbtn_0d_usa and the 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa; 
 CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT NOT NULL 
