2770e3ef0e6e4c50eef7f669eade56371226b945
nnejiobioma <nnejiobioma@gmail.com>
2023-05-18 21:26:20 +0000

    task two amended

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,6 @@
 -- This script that creates the database hbtn_0d_2 and the user user_0d_2.
--- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
--- The user_0d_2 password should be set to user_0d_2_pwd
--- If user and Database already exist code should still run
-
-database="hbtn_0d_2"
-user="user_0d_2"
-password="user_0d_2_pwd"
-
-CREATE DATABASE IF NOT EXISTS $database;
-CREATE USER IF NOT EXISTS '$user'@'localhost' IDENTIFIED BY '$password'; 
-GRANT SELECT ON $database.* TO '$user'@'localhost';"
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' 
+IDENTIFIED BY 'user_0d_2_pwd'; 
+GRANT SELECT ON hbtn_0d_2.* 
+TO 'user_0d_2'@'localhost';"
