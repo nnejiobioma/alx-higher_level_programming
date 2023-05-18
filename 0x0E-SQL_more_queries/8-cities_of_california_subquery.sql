d1ee565e082db64fa8c72839c2311288cabac15c
nnejiobioma <nnejiobioma@gmail.com>
2023-05-18 21:32:35 +0000

    task eight handled

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- This list all cities in the DB
+SELECT cities.* FROM cities, states 
+WHERE cities.state_id = (
+        SELECT id FROM states WHERE name = 'California'
+        ) 
+ORDER BY cities.id ASC;
