d0dc51e57fb328df81de7a9304d66c6ad162a16d
nnejiobioma <nneji.obioma@gmail.com>
2023-07-02 12:42:28 -0700

    Addede file to task five

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in URL sends POST request, displays response
-curl -s -X POST -d "$1" "email=test@gmail.com&subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
