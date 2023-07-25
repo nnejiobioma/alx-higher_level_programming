5e5ea502a95f0b653d1f6efe976063440259b15d
obiomanneji <nneji.obioma@gmail.com>
2023-07-25 12:56:45 -0700

    task two handled

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const requestURL = process.argv[2];
+
+request(requestURL, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  console.log(`code: ${response.statusCode}`);
+});
