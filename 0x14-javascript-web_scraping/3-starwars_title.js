a7cb7051a412100bafbccd0379329640eae9125a
obiomanneji <nneji.obioma@gmail.com>
2023-07-25 13:14:28 -0700

    made small changes to code

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
 request(starWarsUri, function (_error, _response, body) {
-    console.log(body.title);
     body = JSON.parse(body);
+    console.log(body.title);
 });
