e67b9607087b7cd02d33c4e470b901f2a21367ff
obiomanneji <nneji.obioma@gmail.com>
2023-07-25 12:51:20 -0700

    task amended again

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-
+const process = require('process');
 const fs = require('fs');
 
-const data = process.argv[2];
+const cont = process.argv[2];
 
-fs.writeFile(process.argv[3], data, 'utf8', (err) => {
+const text = process.argv[3];
+
+fs.writeFile(cont, text, 'utf8', function (err, data) {
   if (err) {
-    return console.log(err);
+    console.log(err);
   }
 });
-
